716181110e7dc694c450fe7ad4b1de048fc49af8
manvik S <manvik55@gmail.com>
2021-08-11 21:33:36 +0530

    django initial

--- first/calc/models.py
+++ first/calc/models.py
@@ -5,3 +5,6 @@ class Events(models.Model):
     name=models.CharField(max_length=100)
     img=models.ImageField(upload_to='pics')
     desc=models.TextField()
+
+    def __str__(self):
+        return self.name
